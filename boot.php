ba0dcfbd6cafaaf45fb2c805aba15b776c351bdc
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-30 20:45:58 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -16,6 +16,8 @@ if (rex::isBackend() && rex::getUser()) {
 rex_extension::register('PACKAGES_INCLUDED', function () {
     if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('uploader[]')) {
         $include_assets = 0;
+        
+        // Prüfen, ob wir uns im Medienpool oder im Uploader befinden
         if (rex_get('page', 'string') == 'mediapool/upload') {
             $this->setProperty('context', 'mediapool_upload');
             $include_assets = 1;
@@ -38,6 +40,77 @@ rex_extension::register('PACKAGES_INCLUDED', function () {
                 // JavaScript-Variablen vor dem schließenden head-Tag einfügen
                 $vars = include(rex_path::addon('uploader') . 'inc/vars.php');
                 $ep->setSubject(str_replace('</head>', $vars . '</head>', $ep->getSubject()));
+                
+                // Wenn wir im Medienpool sind, benötigen wir ein zusätzliches Template für die Integration
+                if (rex_get('page', 'string') == 'mediapool/upload') {
+                    // Hier fügen wir den Dropzone-Container in den Medienpool ein
+                    $search = '<div class="panel-body">';
+                    $replace = '<div class="panel-body">
+                    <!-- Dropzone-Vorlagen-Template -->
+                    <div id="dropzone-preview-template" style="display: none;">
+                      <div class="dz-preview dz-file-preview">
+                        <div class="dz-image"><img data-dz-thumbnail /></div>
+                        <div class="dz-details">
+                          <div class="dz-size"><span data-dz-size></span></div>
+                          <div class="dz-filename"><span data-dz-name></span></div>
+                        </div>
+                        <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+                        <div class="dz-error-message"><span data-dz-errormessage></span></div>
+                        <div class="dz-success-mark">
+                          <svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="27" cy="27" r="25" fill="white" stroke="#000" stroke-width="2"/>
+                            <path fill="none" stroke="#228B22" stroke-width="4" d="M14,27 L22,35 L42,15"/>
+                          </svg>
+                        </div>
+                        <div class="dz-error-mark">
+                          <svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="27" cy="27" r="25" fill="white" stroke="#000" stroke-width="2"/>
+                            <path fill="none" stroke="#D50000" stroke-width="4" d="M17,17 L37,37 M37,17 L17,37"/>
+                          </svg>
+                        </div>
+                      </div>
+                    </div>';
+                    
+                    // Wir suchen nach der Form und fügen unseren Dropzone-Container ein
+                    $search2 = '<div class="form-group" id="rex-mediapool-choose-file">';
+                    $replace2 = '<!-- Uploader Integration -->
+                    <fieldset>
+                        <legend>Dateien hochladen</legend>
+                        <div id="uploader-dropzone" class="uploader-dropzone dropzone"></div>
+                        <div class="uploader-options">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" ' . ($this->getConfig('image-resize-checked') ? 'checked' : '') . ' id="resize-images"> 
+                                            ' . $this->i18n('buttonbar_resize_images') . '
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" ' . ($this->getConfig('filename-as-title-checked') ? 'checked' : '') . ' id="filename-as-title" name="filename-as-title" value="1"> 
+                                            ' . $this->i18n('buttonbar_filename_as_title') . '
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 text-right">
+                                    <button type="button" class="btn btn-primary start">
+                                        <i class="rex-icon rex-icon-upload"></i>
+                                        ' . $this->i18n('uploader_buttonbar_start_upload') . '
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                    
+                    <!-- Original Medienpool Upload -->
+                    <div class="form-group hidden" id="rex-mediapool-choose-file">';
+                    
+                    $content = $ep->getSubject();
+                    $content = str_replace($search, $replace, $content);
+                    $content = str_replace($search2, $replace2, $content);
+                    $ep->setSubject($content);
+                }
             });
         }
     }
