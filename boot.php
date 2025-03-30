9e9ce6edd8b47bdd2fcb169680aca6708ce77034
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-30 20:09:08 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,23 +1,21 @@
 <?php
+// Datei: boot.php
 $addon = rex_addon::get('uploader');
 
 if (rex::isBackend() && rex::getUser()) {
-        rex_perm::register('uploader[]');
-        rex_perm::register('uploader[page]');
+    rex_perm::register('uploader[]');
+    rex_perm::register('uploader[page]');
 
-  if (!rex::getUser()->hasPerm('uploader[page]')) {
-     $page = $this->getProperty('page');
-     $page['hidden'] = 'true';
-     $this->setProperty('page', $page);
-  }
+    if (!rex::getUser()->hasPerm('uploader[page]')) {
+        $page = $this->getProperty('page');
+        $page['hidden'] = 'true';
+        $this->setProperty('page', $page);
+    }
 }    
 
-rex_extension::register('PACKAGES_INCLUDED', function ()
-{
-    if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('uploader[]'))
-    {
-        if (rex::isDebugMode() && rex_request_method() == 'get')
-        {
+rex_extension::register('PACKAGES_INCLUDED', function () {
+    if (rex::isBackend() && rex::getUser() && rex::getUser()->hasPerm('uploader[]')) {
+        if (rex::isDebugMode() && rex_request_method() == 'get') {
             $compiler = new rex_scss_compiler();
             $compiler->setRootDir($this->getPath());
             $compiler->setScssFile($this->getPath('scss/uploader.scss'));
@@ -26,53 +24,32 @@ rex_extension::register('PACKAGES_INCLUDED', function ()
             rex_file::copy($this->getPath('assets/uploader.css'), $this->getAssetsPath('uploader.css'));
             rex_file::copy($this->getPath('assets/uploader.js'), $this->getAssetsPath('uploader.js'));
         }
+        
         $include_assets = 0;
-        if (rex_get('page', 'string') == 'mediapool/upload' )
-        {
+        if (rex_get('page', 'string') == 'mediapool/upload') {
             $this->setProperty('context', 'mediapool_upload');
             $include_assets = 1;
         }
-        /*
-        elseif (rex_get('page', 'string') == 'mediapool/media')
-        {
-            $this->setProperty('context', 'mediapool_media');
-            $include_assets = 1;
-        }
-        */
-        elseif (rex_get('page', 'string') == 'uploader/upload')
-        {
+        elseif (rex_get('page', 'string') == 'uploader/upload') {
             $this->setProperty('context', 'addon_upload');
             $include_assets = 1;
         }
 
-        if ($include_assets)
-        {
-            rex_view::addJsFile($this->getAssetsUrl('vendor/JavaScript-Templates/js/tmpl.min.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/JavaScript-Load-Image/js/load-image.all.min.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.iframe-transport.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-process.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-image.js'));
-            //rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-audio.js'));
-            //rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-video.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-validate.js'));
-            rex_view::addJsFile($this->getAssetsUrl('vendor/jquery-file-upload/js/jquery.fileupload-ui.js'));
-            rex_view::addCssFile($this->getAssetsUrl('vendor/jquery-file-upload/css/jquery.fileupload.css'));
-            rex_view::addCssFile($this->getAssetsUrl('vendor/jquery-file-upload/css/jquery.fileupload-ui.css'));
+        if ($include_assets) {
+            // Dropzone.js und CSS per CDN einbinden
+            rex_view::addCssFile('https://unpkg.com/dropzone@5/dist/min/dropzone.min.css');
+            rex_view::addJsFile('https://unpkg.com/dropzone@5/dist/min/dropzone.min.js');
+            
+            // Eigene CSS- und JS-Dateien
             rex_view::addCssFile($this->getAssetsUrl('uploader.css'));
             rex_view::addJsFile($this->getAssetsUrl('uploader.js'));
 
-            rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep)
-            {
+            rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
                 $buttonbar_template = include(rex_path::addon('uploader') . 'inc/buttonbar.php');
                 $ep->setSubject(str_replace('</body>', $buttonbar_template . '</body>', $ep->getSubject()));
                 $vars = include(rex_path::addon('uploader') . 'inc/vars.php');
-                $file_list_templates = include(rex_path::addon('uploader') . 'inc/filelists.php');
-                $ep->setSubject(str_replace('</head>', $file_list_templates . $vars . '</head>', $ep->getSubject()));
+                $ep->setSubject(str_replace('</head>', $vars . '</head>', $ep->getSubject()));
             });
         }
-
     }
 }, rex_extension::LATE);
-
